31dfd65cd869fbb3f0c847137c393e76382f295f
Jeiso Guerrero <jei_son1516@hotmail.com>
2024-05-06 01:07:50 -0400

    confugure dap, and add mason debug lug

--- lua/lazy-bootstrap.lua
+++ lua/lazy-bootstrap.lua
@@ -374,8 +374,9 @@ require('lazy').setup({
       --    :Mason
       --
       --  You can press `g?` for help in this menu.
-      require('mason').setup()
-
+      require('mason').setup {
+        log_level = vim.log.levels.DEBUG,
+      }
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
