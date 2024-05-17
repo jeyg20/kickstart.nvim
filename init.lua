a3aac397bf6e9fc5ceaecdaf8d4edd4e546667eb
Jeiso Guerrero <jei_son1516@hotmail.com>
2024-05-17 14:10:57 -0400

    set virtual interpreter virtual enviroment

--- init.lua
+++ init.lua
@@ -4,23 +4,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
+vim.g.python3_host_prog = './env/bin/python3.11'
 
 require 'options'
 require 'keymaps'
 require 'lazy-bootstrap'
--- Set to true if you have a Nerd Font installed and selected in the terminal
-
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
