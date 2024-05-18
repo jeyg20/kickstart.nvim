fbe2f3dca9bf23d1e9ba7d75e20f65bac21d8a75
Jeiso Guerrero <jei_son1516@hotmail.com>
2024-05-18 06:05:11 -0400

    Update comflicting keymaps

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -31,7 +31,7 @@ return {
       end,
     })
 
-    vim.keymap.set('n', 'gu', '<cmd>diffget //2<CR>')
-    vim.keymap.set('n', 'gh', '<cmd>diffget //3<CR>')
+    vim.keymap.set('n', 'gdu', '<cmd>diffget //2<CR>')
+    vim.keymap.set('n', 'gdh', '<cmd>diffget //3<CR>')
   end,
 }
