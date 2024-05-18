e2d491a970218bbb9010bc1053c46114a8797006
Jeiso Guerrero <jei_son1516@hotmail.com>
2024-05-18 06:04:24 -0400

    Add keymaps for python unit testing

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -64,3 +64,15 @@ vim.api.nvim_create_autocmd('CursorHold', {
     vim.diagnostic.open_float(nil, { scope = 'line', border = 'rounded' })
   end,
 })
+
+vim.keymap.set('n', '<leader>tc', function()
+  if vim.bo.filetype == 'python' then
+    require('dap-python').test_class()
+  end
+end)
+
+vim.keymap.set('n', '<leader>tm', function()
+  if vim.bo.filetype == 'python' then
+    require('dap-python').test_method()
+  end
+end)
