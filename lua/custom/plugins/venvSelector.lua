7b17fd7e11e3756e85658aa4fbabb08413b95274
Jeiso Guerrero <jei_son1516@hotmail.com>
2024-07-17 23:59:42 -0400

    Add python venv selector plugin

---
+++ lua/custom/plugins/venvSelector.lua
@@ -0,0 +1,25 @@
+return {
+  'linux-cultist/venv-selector.nvim',
+  dependencies = {
+    'neovim/nvim-lspconfig',
+    'mfussenegger/nvim-dap',
+    'mfussenegger/nvim-dap-python', --optional
+    { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } },
+  },
+  lazy = false,
+  branch = 'regexp', -- This is the regexp branch, use this for the new version
+  config = function()
+    require('venv-selector').setup {
+      settings = {
+        search = {
+          mamba_envs = {
+            command = "find ~/anaconda3/envs/ -type d -name 'bin' -exec test -e '{}/python' ';' -print",
+          },
+        },
+      },
+    }
+  end,
+  keys = {
+    { ',v', '<cmd>VenvSelect<cr>' },
+  },
+}
