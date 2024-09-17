c47b282713df0f1846cc822967dc09145b10e1aa
Jeiso Guerrero <jei_son1516@hotmail.com>
2024-09-17 04:15:46 -0400

    add query to treesitter parser

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'htmldjango', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'vim', 'vimdoc', 'python' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
