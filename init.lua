e3f5fa6bb8aedeff1408247fd3d2224227bff429
jeison <jei_son1516@hotmail.com>
2025-04-18 19:26:06 -0400

    delete deprecated code

--- init.lua
+++ init.lua
@@ -1,14 +1,24 @@
 -- Set <space> as the leader key
 -- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.python3_host_prog = '/home/jeison/miniconda3/bin/python'
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = true
+vim.g.have_nerd_font = false
 
 -- [[ Setting options ]]
 require 'options'
+
+-- [[ Basic Keymaps ]]
 require 'keymaps'
+
+-- [[ Install `lazy.nvim` plugin manager ]]
 require 'lazy-bootstrap'
+
+-- [[ Configure and install plugins ]]
+require 'lazy-plugins'
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
