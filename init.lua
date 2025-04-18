7815fa858fb7973f6401541686791dda823dcb09
Jeison <jei_son1516@hotmail.com>
2025-04-18 01:55:38 -0400

    update wrapping options

--- init.lua
+++ init.lua
@@ -3,6 +3,9 @@
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.g.python3_host_prog = '/home/jeison/anaconda3/envs/conda_env/bin/python'
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
 vim.g.python3_host_prog = '/home/jeison/anaconda3/envs/env/bin/python'
